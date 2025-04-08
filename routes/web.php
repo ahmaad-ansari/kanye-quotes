b87bc544f492ecefce5ccfc6bdee6c58708a679d
Ahmaad Ansari <ahmaad.ali.ansari@hotmail.com>
2025-04-07 22:51:39 -0400

    Created all routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['password.protection'])->group(function () {
+    Route::get('/', [QuoteController::class, 'index'])->name('quote.index');
 });
